733a8f558d5b628ceab15e3034840eda62fea4f6
Ionut Cora <cora.ionut92@yahoo.com>
2023-11-28 15:48:32 +0000

    contact page header

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,7 +3,13 @@ import './Contact.css';
 
 function Contact() {
   return (
-    <div className='mt-5'>Contact</div>
+    <div className='contact-page'>
+      <header className='height-50 mt-5'>
+        <div className="container h-100 d-flex align-items-center justify-content-center">
+          <h1 className='text-light'>Contact</h1>
+        </div>
+      </header>
+    </div>
   )
 }
 
