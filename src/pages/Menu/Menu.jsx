d2a31b50b9e2236141cea7edd9c076be8e41de8d
Ionut Cora <cora.ionut92@yahoo.com>
2023-11-28 13:49:43 +0000

    menu page header

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -3,7 +3,13 @@ import './Menu.css';
 
 function Menu() {
   return (
-    <div className='mt-5'>Menu</div>
+    <div className='menu-page'>
+      <header className='height-50 mt-5'>
+        <div className='container h-100 d-flex align-items-center justify-content-center'>
+          <h1 className='text-light'>Menu</h1>
+        </div>
+      </header>
+    </div>
   )
 }
 
