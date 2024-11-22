05c339b97d76c555a016baeff719446cdee5de51
Ionut Cora <cora.ionut92@yahoo.com>
2024-11-22 17:58:42 +0000

    navbar center

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ function App() {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basiv-navbar-nav'>
-            <Nav className='me-auto justify-content-end w-100'>
-              <Link to='/' className='nav-link active text-uppercase'>Home</Link>
+            <Nav className='me-auto justify-content-center w-100'>
+              <Link to='/' className='nav-link text-uppercase'>Home</Link>
               <Link to='/menu' className='nav-link text-uppercase'>Menu</Link>
               <Link to='/about' className='nav-link text-uppercase'>About</Link>
               <Link to='/contact' className='nav-link text-uppercase'>Contact</Link>
