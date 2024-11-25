bd57109915bbfcda757ca702433430ba3ec52a41
Ionut Cora <cora.ionut92@yahoo.com>
2024-11-25 12:45:36 +0000

    reviews motion add

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,37 +1,8 @@
 import React from 'react';
 import './Reviews.css';
 import { Card, CardBody, CardText, CardFooter, CardTitle } from 'react-bootstrap';
-import Person1Img from '../../utils/images/person1-img.jpg';
-import Person2Img from '../../utils/images/person2-img.jpg';
-import Person3Img from '../../utils/images/person3-img.jpg';
-import Person4Img from '../../utils/images/person4-img.jpg';
-
-const reviews = [
-    {
-        id: 1,
-        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium itaque facere ratione libero expedita non.',
-        img: [Person1Img],
-        name: 'John Mike'
-    },
-    {
-        id: 2,
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, voluptatum!',
-        img: [Person2Img],
-        name: 'Maria Cruz'
-    },
-    {
-        id: 3,
-        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime deleniti quam, voluptates et non mollitia eveniet. Iste quidem officiis doloribus!',
-        img: [Person3Img],
-        name: 'Anna Gold'
-    },
-    {
-        id: 4,
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quam accusantium, aliquam incidunt fugit est.',
-        img: [Person4Img],
-        name: 'Nick Burn'
-    }
-];
+import { motion } from 'framer-motion';
+import reviews from '../../utils/reviews';
 
 function Reviews() {
   return (
@@ -39,7 +10,13 @@ function Reviews() {
         <h2 className='text-center mb-5 text-uppercase fw-bold fs-1'>Reviews</h2>
         <div className="row g-4">
             {reviews.map((review) => (
-                <div key={review.id} className='col-lg-6'>
+                <motion.div 
+                    key={review.id} 
+                    className='col-lg-6'
+                    initial={{ opacity: 0, x: `${review.initialMotion}` }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 1 }}
+                >
                     <Card className="h-100 shadow">
                         <CardBody>
                             <div className="p-4">
@@ -51,7 +28,7 @@ function Reviews() {
                             <CardTitle className="text-success">{review.name}</CardTitle>
                         </CardFooter>
                     </Card>
-                </div>
+                </motion.div>
             ))}
         </div>
     </div>
---
+++ src/utils/reviews.js
@@ -0,0 +1,37 @@
+import Person1Img from './images/person1-img.jpg';
+import Person2Img from './images/person2-img.jpg';
+import Person3Img from './images/person3-img.jpg';
+import Person4Img from './images/person4-img.jpg';
+
+const reviews = [
+    {
+        id: 1,
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium itaque facere ratione libero expedita non.',
+        img: [Person1Img],
+        name: 'John Mike',
+        initialMotion: '-300px'
+    },
+    {
+        id: 2,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, voluptatum!',
+        img: [Person2Img],
+        name: 'Maria Cruz',
+        initialMotion: '350px'
+    },
+    {
+        id: 3,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime deleniti quam, voluptates et non mollitia eveniet. Iste quidem officiis doloribus!',
+        img: [Person3Img],
+        name: 'Anna Gold',
+        initialMotion: '-300px'
+    },
+    {
+        id: 4,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quam accusantium, aliquam incidunt fugit est.',
+        img: [Person4Img],
+        name: 'Nick Burn',
+        initialMotion: '350px'
+    }
+];
+
+export default reviews;
