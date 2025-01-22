297f2a98c05476285518e0027d259d1bd8a8c8df
Ionut Cora <cora.ionut92@yahoo.com>
2025-01-22 11:05:43 +0000

    about page text and style changes

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './About.css';
+import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import AboutChef1Img from '../../utils/images/about-chef1-img.jpg';
-import AboutChef2Img from '../../utils/images/about-chef2-img.jpg';
 import ImageGallery from '../../components/ImageGallery/ImageGallery';
 import Reviews from '../../components/Reviews/Reviews';
 
@@ -21,17 +21,18 @@ function About() {
       </header>
 
       <div className="container my-5">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, consectetur. Possimus dolorem ratione illo fugiat exercitationem sunt aperiam, reprehenderit quo repudiandae excepturi. Doloribus veritatis quasi incidunt sequi aspernatur necessitatibus ea fugiat quod vero repellat assumenda quae odio quas enim deserunt consequatur, inventore illum doloremque, esse autem. Atque dolor veritatis sequi?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quidem placeat inventore, illum commodi eum voluptas praesentium obcaecati at minima. Facere, praesentium velit amet iure quod sunt deserunt delectus laboriosam esse. Saepe excepturi at sed eius in aspernatur voluptatibus iure?</p>
-
         <div className="row">
           <motion.div 
-            className="col-lg-6"
+            className="col-lg-6 d-flex flex-column justify-content-center mb-5 mb-lg-0"
             initial={{ opacity: 0, x: -300 }}
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: 1 }}
           >
-            <img src={AboutChef1Img} className='img-fluid my-4' alt="" />
+            <p>Welcome to Korlake Restaurant in London, the perfect experience for special people. Our fancy restaurant will bring your special moments to another level.</p>
+            <p>Our restaurant is situated at 5 minutes walk from train station and shopping center. The location is ideal for a perfect breakfast, a facny lunch or a nice dinner with your family or friends. We can't wait to serve you our delicious dishes!</p>
+            <Link to='/contact'>
+              <button type='button' className='btn btn-success btn-lg rounded-0 text-capitalize mt-3 shadow'>Contact us</button>
+            </Link>
           </motion.div>
           <motion.div 
             className="col-lg-6"
@@ -39,11 +40,9 @@ function About() {
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: 1 }}
           >
-            <img src={AboutChef2Img} className='img-fluid my-4' alt="" />
+            <img src={AboutChef1Img} className='img-fluid' alt="our staff" />
           </motion.div>
         </div>
-
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis eos sapiente voluptate odit mollitia optio ducimus id consequatur molestiae deleniti qui repellat commodi consectetur a, voluptates autem adipisci! Aperiam incidunt in placeat et illo molestiae, porro vel, sapiente provident doloremque, similique harum blanditiis quibusdam qui!</p>
       </div>
 
       <div className="bg-dark text-light py-1">
