5e8115dfada852c5de27fcce26ba232fc8ea1a53
Ionut Cora <cora.ionut92@yahoo.com>
2025-01-22 10:55:02 +0000

    home page about section text change

--- src/components/AboutUsSection/AboutUsSection.jsx
+++ src/components/AboutUsSection/AboutUsSection.jsx
@@ -23,8 +23,8 @@ function AboutUsSection() {
                 transition={{ duration: 1 }}
             >
                 <h2 className='text-uppercase fw-bold fs-1 mb-4 mb-lg-5'>About us</h2>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi nesciunt sed maxime, nemo nulla velit. Dolor, fuga fugit! Impedit dolores quisquam distinctio quas, nemo et.</p>
-                <p className='mb-4 mb-lg-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos similique sed saepe fugiat laudantium ipsam ea rem magni et eveniet aut, iusto officia, fugit nisi veniam consequuntur? Maxime minus blanditiis repudiandae repellat quam velit nisi?</p>
+                <p>Welcome to Korlake Restaurant in London, the perfect experience for special people. Our fancy restaurant will bring your special moments to another level.</p>
+                <p className='mb-4 mb-lg-5'>Our restaurant is situated at 5 minutes walk from train station and shopping center. The location is ideal for a perfect breakfast, a facny lunch or a nice dinner with your family or friends. We can't wait to serve you our delicious dishes!</p>
                 <Link to='/about'>
                     <button type='button' className='btn btn-dark btn-lg rounded-0 text-capitalize shadow'>More about us</button>
                 </Link>
